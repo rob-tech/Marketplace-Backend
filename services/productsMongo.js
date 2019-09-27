9978e08a2da13b3812fe797acc53f1ed73c157e1
rob <robertahuber88@gmail.com>
2019-09-27 16:07:03 +0200

    last commit

--- services/productsMongo.js
+++ services/productsMongo.js
@@ -1,91 +1,151 @@
 const express = require("express");
 const idGen = require("shortid");
-const { MongoClient, ObjectID} = require('mongodb')
+const { MongoClient, ObjectID } = require('mongodb')
 // const productschema = require('./schema')
 
 const mongoServerURL = 'mongodb://localhost:27017'
 
 getProducts = async (filter = {}) => {
-    try {
-      const mongo = await MongoClient.connect(mongoServerURL, {
-        useNewUrlParser: true
-      })
-      const collection = mongo.db("Amazon").collection("Products")
-      const products = collection.find(filter).toArray()
-      return products ? products : []
-    } catch (error) {
-      console.log(error)
-    }
+  try {
+    const mongo = await MongoClient.connect(mongoServerURL, {
+      useNewUrlParser: true
+    })
+    const collection = mongo.db("Amazon").collection("Products")
+    const products = collection.find(filter).toArray()
+    return products ? products : []
+  } catch (error) {
+    console.log(error)
   }
-  
-  const router = express.Router();
+}
 
-  router.get("/", async (req, res) => {
-    const products = await getProducts(req.query);
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const products = await getProducts(req.query);
+  res.send(products)
+})
+
+router.get("/:id", async (req, res) => {
+  try {
+    const products = await getProducts({ _id: new ObjectID(req.params.id) });
     res.send(products)
-  })
+  } catch (error) {
+    res.send("id not found")
+  }
+})
 
-  router.get("/:id", async (req, res) => {
-    try {
-      const products = await getProducts({ _id: new ObjectID(req.params.id) });
-      res.send(products)
-    } catch (error) {
-      res.send("id not found")
+router.post("/", async (req, res) => {
+  try {
+    const mongo = await MongoClient.connect(mongoServerURL, {
+      useNewUrlParser: true
+    })
+    const collection = mongo.db("Amazon").collection("Products")
+    req.body.creationDate = new Date()
+    req.body.updatedDate = req.body.creationDate
+    const { insertedId } = await collection.insertOne(req.body)
+    res.send(insertedId)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const mongo = await MongoClient.connect(mongoServerURL, {
+//       useNewUrlParser: true
+//     })
+
+//     const collection = mongo.db("Amazon").collection("Products")
+//     var modification = req.body
+//     const { modifiedCount } = await collection.updateOne({ _id: req.params.id }, { $set: req.body })
+
+//     if (modifiedCount > 0) {
+//       res.send('OK')
+//     } else {
+//       res.send('NOTHING TO MODIFY')
+//     }
+
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
+
+
+router.put("/:id", async (req, res) => {
+  try {
+    const mongo = await MongoClient.connect(mongoServerURL, {
+      useNewUrlParser: true
+    })
+    const collection = mongo.db("Amazon").collection("Products")
+    var modification = req.body
+    var newProduct = await collection.updateOne({ _id: req.params.id }, { $set: modification })
+    console.log(newProduct)
+    res.send(modification)
+  } catch (error) {
+    res.send(error)
+    console.log(error)
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const mongo = await MongoClient.connect(mongoServerURL, {
+      useNewUrlParser: true
+    })
+    const collection = mongo.db("Amazon").collection("Products")
+    const { deletedCount } = await collection.deleteOne({ _id: new ObjectID(req.params.id) });
+    if (deletedCount > 0) {
+      res.send('OK')
+    } else {
+      res.send('ID not found')
     }
-  })
+  } catch (error) {
+  }
+})
 
-  router.post("/", async (req, res) => {
-    try {
-      const mongo = await MongoClient.connect(mongoServerURL, {
-        useNewUrlParser: true
-      })
-      const collection = mongo.db("Amazon").collection("Products")
-      req.body.creationDate = new Date()
-      req.body.updatedDate = req.body.creationDate
-      const { insertedId } = await collection.insertOne(req.body)
-      res.send(insertedId)
-    } catch (error) {
-      res.send(error)
-    }
-  })
+///////REVIEWS////////////
 
-  router.put("/:id", async (req, res) => {
-    try {
-        const mongo = await MongoClient.connect(mongoServerURL, {
-          useNewUrlParser: true
-        })
+router.get("/:id/reviews", async (req, res) => {
+  try {
+    const product = await getProducts({ _id: new ObjectID(req.params.id) });
+    var review = product[0].reviews
+    res.send(review)
+  } catch (error) {
+    res.send("id not found")
+    console.log(error)
+  }
+})
+
+router.post("/:id/reviews", async (req, res) => {
+  try {
+    const mongo = await MongoClient.connect(mongoServerURL, {
+      useNewUrlParser: true
+    })
+    const collection = mongo.db("Amazon").collection("Products")
+    req.body.id = new ObjectID();
+    const review = await collection.updateOne({ _id: new ObjectID(req.params.id)}, {$push: {reviews: req.body}} )
+    res.send(review)    
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.delete("/:id/reviews/:reviewId", async (req, res) => {
+  try {
+    const mongo = await MongoClient.connect(mongoServerURL, {
+      useNewUrlParser: true
+    })
+    const collection = mongo.db("Amazon").collection("Products")
+    console.log(collection)
+    const review = await collection.updateOne({ _id: new ObjectID(req.params.id)}, {$pull: {reviews: { _id: req.params.reviewId} } } )
+    console.log(review)
+    res.send( "OK" )
     
-        const collection = mongo.db("Amazon").collection("Products")
-    
-        const { modifiedCount } = await collection.updateOne({ _id: req.params.id })
+  } catch (error) {
+    console.log(error)
+  }
 
-    
-        if (modifiedCount > 0) {
-          res.send('OK')
-        } else {
-          res.send('NOTHING TO MODIFY')
-        }
-    
-      } catch (error) {
-    console.log
-      }
-  })
-  
-  router.delete("/:id", async (req, res) => {
-    try {
-      const mongo = await MongoClient.connect(mongoServerURL, {
-        useNewUrlParser: true
-      })
-      const collection = mongo.db("Amazon").collection("Products")
-      const { deletedCount } = await collection.deleteOne({ _id: new ObjectID(req.params.id) });
-      if (deletedCount > 0) {
-        res.send('OK')
-      } else {
-        res.send('ID not found')
-      }
-    } catch (error) {
-    }
-  })
+})
 
 
-  module.exports = router;
+module.exports = router;
